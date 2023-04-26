9ca7b8cb9ca240fc74412029830f24e331b7f329
Larisa-Pomidor <nikakinikakin@gmail.com>
2023-04-26 20:53:18 +0300

    Initializing 2SPL Portfolio

---
+++ variations.php
@@ -0,0 +1,315 @@
+<?php
+$var = array(
+    "en" => array(
+        "name" => array(
+            "woman" => "Larisa Pomidor",
+            "man" => "Hot Puss Invader"
+        ),
+        "main-btn" => "Contact Me",
+        "menu" => array(
+            "home" => "Meet Me",
+            "about" => "About",
+            "services" => "Services",
+            "experience" => "Experience",
+            "portfolio" => "Portfolio",
+            "contacts" => "Contacts"
+        ),
+        "scroll" => "Scroll Down",
+        "about-title" => "About Me",
+        "about-text" => "
+            <b>A certified professional</b> in the field of web development.
+            I have 7 years of experience creating high-quality websites, with about 500 responsive pages across all devices,
+            featuring unique and concise designs.
+            <br><br>
+            <b>My skills and experience</b> enable me to work with many programming languages and technologies,
+            such as HTML, CSS, JavaScript, PHP, Python, Java, React, Vue.js, and many others. I also have experience working with various CMS and
+            web builders.
+            <br><br>
+            I carefully listen to clients' needs, adapt to their business models, and provide individual solutions.
+            <b>I create projects that produce results and know what really works.</b> I can help bring your idea to the world!
+        ",
+        "services-title" => "Services",
+        "services" => array(
+            "1" => array(
+                "name" => "Design",
+                "desc" => "Complete copying of an existing website or creating a visual and functional layout for a website from scratch based on your template"
+            ),
+            "2" => array(
+                "name" => "Turnkey website",
+                "desc" => "Development of adaptive online stores, landing pages, business card websites on popular platforms, and integration with a variety of modern CRMs"
+            ),
+            "3" => array(
+                "name" => "Improvement",
+                "desc" => "Fixing errors made by previous developers, making changes, content filling, redesigning and setting up the website to work correctly"
+            ),
+            "4" => array(
+                "name" => "Further support",
+                "desc" => "Free consultation on website operation. Taking your business to a new level. Long-term relationships with customers. Always in touch!"
+            ),
+            "5" => array(
+                "name" => "SEO promotion",
+                "desc" => "Website optimization for popular search engines, structuring all web page sections for search engine robots"
+            ),
+            "6" => array(
+                "name" => "Additional services",
+                "desc" => "Development of unique useful elements for your website: calculators, quizzes, mini-games, questionnaires, payment systems"
+            )
+        ),
+        "custom" => "But the magic doesn't end there :) Describe your task, and I'll be sure to help you with it!",
+        "experience-title" => "Experience",
+        "experience" => array(
+            "study" => array(
+                [
+                    "year" => "2020 - Present",
+                    "title" => "Programming courses",
+                    "desc" => "Taking programming courses to further advance my skills and knowledge"
+                ],
+                [
+                    "year" => "2018 - 2020",
+                    "title" => "Master's degree",
+                    "desc" => "Earned a Master's degree in Applied Mathematics,
+                    focusing on applying programming technologies to model mathematical processes"
+                ],
+                [
+                    "year" => "2014 - 2018",
+                    "title" => "Bachelor's degree",
+                    "desc" => "Received a Bachelor's degree in Computer Science and Programming"
+                ]
+            ),
+            "work" => array(
+                [
+                    "year" => "2021 - Present",
+                    "title" => "Back-End Developer",
+                    "desc" => "Currently focusing on developing and maintaining the back-end of web applications"
+                ],
+                [
+                    "year" => "2018 - Present",
+                    "title" => "Front-End Developer",
+                    "desc" => "Specializing in creating responsive websites for companies of different sizes and industries"
+                ],
+                [
+                    "year" => "2014 - Present",
+                    "title" => "Web Designer",
+                    "desc" => "Creating concise designs across various platforms and developing templates for Bitrix24 service"
+                ]
+            )
+        ),
+        "portfolio-title" => "Recent works",
+        "contacts-title" => "Contacts",
+        "contacts-subtitle" => "Let's cooperate right now!",
+        "contacts-text" => "To reach me faster, please message me on",
+        "form" => array(
+            "name" => "Your name",
+            "message" => "Your message",
+            "button" => "Send message"
+        )
+    ),
+
+    "ru" => array(
+        "name" => array(
+            "woman" => "Лариса Помидор",
+            "man" => "Тима Котеночек"
+        ),
+        "main-btn" => "Связаться",
+        "menu" => array(
+            "home" => "Знакомство",
+            "about" => "Обо мне",
+            "services" => "Услуги",
+            "experience" => "Опыт",
+            "portfolio" => "Портфолио",
+            "contacts" => "Контакты"
+        ),
+        "scroll" => "Подробнее",
+        "about-title" => "Обо мне",
+        "about-text" => "
+            <b> Дипломированный специалист</b> в области веб-разработки. За моей спиной 7 лет создания качественных сайтов, реализовано
+            порядка 500 адаптивных под все устройства страниц с уникальным и лаконичным дизайном.
+            <br><br>
+            <b>Мои навыки и опыт</b> позволяют мне работать со многими технологиями и языками программирования, такими как HTML, CSS, JavaScript, PHP,
+            Python, Java, React, Vue.js и многими другими. Я также имею опыт работы с различными CMS и конструкторами.
+            <br><br>
+            Внимательно прислушиваюсь к потребностям клиентов, адаптируюсь к их бизнес-моделям и предоставляю индивидуальные решения.
+            <b>Создаю нужные проекты и знаю, что действительно приносит результат.</b> Помогу донести вашу идею всему миру!
+        ",
+        "services-title" => "Услуги",
+        "services" => array(
+            "1" => array(
+                "name" => "Дизайн",
+                "desc" => "Полное копирование существующего сайта или отрисовка и верстка уникального анимированного дизайна с нуля / по вашему шаблону"
+            ),
+            "2" => array(
+                "name" => 'Сайт "под ключ"',
+                "desc" => "Разработка адаптивных интернет-магазинов, лендингов, сайтов-визиток на популярных платформах и интеграция со множеством современных CRM"
+            ),
+            "3" => array(
+                "name" => "Доработка",
+                "desc" => "Исправление ошибок предыдущих разработчиков, внесение правок, наполнение контентом, редизайн и налаживание корректной работы сайта"
+            ),
+            "4" => array(
+                "name" => "Дальнейшая поддержка",
+                "desc" => "Бесплатная консультация по работе сайтов. Выведение бизнеса на новый уровень. Долгосрочные отношения с клиентами. Всегда на связи!"
+            ),
+            "5" => array(
+                "name" => "SEO продвижение",
+                "desc" => "Оптимизация сайта под популярные поисковые системы, структуризация всех разделов веб-страниц для робота-поисковика"
+            ),
+            "6" => array(
+                "name" => "Дополнительно",
+                "desc" => "Разработка уникальных полезных мелочей для вашего сайта: калькуляторов, квизов, мини-игр, опросников, платёжных систем"
+            )
+        ),
+
+        "custom" => "На этом магия не заканчивается :) Опишите свою задачу, и я обязательно Вам помогу!",
+        "experience-title" => "Опыт",
+        "experience" => array(
+            "study" => array(
+                [
+                    "year" => "2020 - Настоящее время",
+                    "title" => "Курсы по программированию",
+                    "desc" => "Прохожу курсы по программированию, чтобы дополнительно повысить свои навыки и знания"
+                ],
+                [
+                    "year" => "2018 - 2020",
+                    "title" => "Степень магистра",
+                    "desc" => "Получена степень магистра прикладной математики с уклоном на применение
+                    технологий программирования для моделирования математических процессов"
+                ],
+                [
+                    "year" => "2014 - 2018",
+                    "title" => "Степень бакалавра",
+                    "desc" => "Получена степень бакалавра в области компьютерных наук и программирования"
+                ]
+            ),
+            "work" => array(
+                [
+                    "year" => "2021 - Настоящее время",
+                    "title" => "Back-End Разработчик",
+                    "desc" => "В настоящее время фокусируюсь на разработке и поддержке бэкенда веб-приложений"
+                ],
+                [
+                    "year" => "2018 - Настоящее время",
+                    "title" => "Front-End Разработчик",
+                    "desc" => "Специализируюсь на создании адаптивных веб-сайтов для компаний различных размеров и отраслей"
+                ],
+                [
+                    "year" => "2014 - Настоящее время",
+                    "title" => "Web Дизайнер",
+                    "desc" => "Создаю лаконичные дизайны на разных платформах и разрабатываю шаблоны для сервиса Bitrix24"
+                ]
+            )
+        ),
+        "portfolio-title" => "Последние работы",
+        "contacts-title" => "Контакты",
+        "contacts-subtitle" => "Давайте сотрудничать прямо сейчас!",
+        "contacts-text" => "Чтобы быстрее связаться со мной, пишите мне в",
+        "form" => array(
+            "name" => "Ваше имя",
+            "message" => "Ваше сообщение",
+            "button" => "Ваше сообщение"
+        )
+    ),
+
+    "uk" => array(
+        "name" => array(
+            "woman" => "Лариса Помідор",
+            "man" => "Тіма Кошенятко"
+        ),
+        "main-btn" => "Зв'язатись",
+        "menu" => array(
+            "home" => "Знайомство",
+            "about" => "Про мене",
+            "services" => "Послуги",
+            "experience" => "Досвід",
+            "portfolio" => "Портфоліо",
+            "contacts" => "Контакти"
+        ),
+        "scroll" => "Детальніше",
+        "about-title" => "Про мене",
+        "about-text" => "
+            <b>Дипломований фахівець</b> в галузі веб-розробки. За моєю спиною 7 років створення якісних веб-сайтів,
+            реалізовано близько 500 адаптивних сторінок для всіх пристроїв з унікальним та лаконічним дизайном.
+            <br><br>
+            <b>Мої навички та досвід</b> дозволяють мені працювати з багатьма мовами програмування та технологіями,
+            такими як HTML, CSS, JavaScript, PHP, Python, Java, React, Vue.js та багатьма іншими.
+            Також маю досвід роботи з різними CMS та конструкторами.
+            <br><br>
+            Я уважно слухаю потреби клієнтів, адаптуюсь до їх бізнес-моделей та надаю індивідуальні рішення.
+            <b>Створюю круті проекти та знаю, що дійсно приносить результат. </b>
+            Допоможу донести вашу ідею всьому світу!
+        ",
+        "services-title" => "Послуги",
+        "services" => array(
+            "1" => array(
+                "name" => "Дизайн",
+                "desc" => "Повноцінне копіювання існуючого сайту або створення унікального анімованого дизайну з нуля / за вашим шаблоном в короткі терміни"
+            ),
+            "2" => array(
+                "name" => 'Сайт "під ключ"',
+                "desc" => "Розробка адаптивних інтернет-магазинів, лендінгів, сайтів-візиток на популярних платформах та інтеграція з багатьма сучасними CRM"
+            ),
+            "3" => array(
+                "name" => "Доробка",
+                "desc" => "Виправлення помилок попередніх розробників, внесення правок, наповнення контентом, редизайн та налаштування правильної роботи сайту"
+            ),
+            "4" => array(
+                "name" => "Подальша підтримка",
+                "desc" => "Безкоштовна консультація з роботи сайтів. Виведення бізнесу на новий рівень. Довгострокові відносини з клієнтами. Завжди на зв'язку!"
+            ),
+            "5" => array(
+                "name" => "SEO оптимізація",
+                "desc" => "Оптимізація сайту під популярні пошукові системи, структуризація всіх розділів веб-сторінок для робота-пошуковика"
+            ),
+            "6" => array(
+                "name" => "Додатково",
+                "desc" => "Розробка унікальних корисних елементів для вашого сайту: калькуляторів, квизів, міні-ігор, опитувань, платіжних систем"
+            )
+        ),
+        "portfolio-title" => "Останні роботи",
+        "custom" => "На цьому магія не закінчується :) Опишіть своє завдання, і я обов'язково Вам допоможу!",
+        "experience-title" => "Досвід",
+        "experience" => array(
+            "study" => array(
+                [
+                    "year" => "2020 - Нині",
+                    "title" => "Курси з программування",
+                    "desc" => "Проходжу курси з програмування, щоб додатково покращити свої навички та знання"
+                ],
+                [
+                    "year" => "2018 - 2020",
+                    "title" => "Ступінь магістра",
+                    "desc" => "Отримана ступінь магістра прикладної математики з уклоном на використання технологій програмування для моделювання математичних процесів"
+                ],
+                [
+                    "year" => "2014 - 2018",
+                    "title" => "Ступень бакалавра",
+                    "desc" => "Отримана ступінь бакалавра з комп’ютерних наук та програмування"
+                ]
+            ),
+            "work" => array(
+                [
+                    "year" => "2021 - Нині",
+                    "title" => "Back-End Розробник",
+                    "desc" => "В даний час фокусуюся на розробці та підтримці бекенду веб-додатків та мобільних застосунків"
+                ],
+                [
+                    "year" => "2018 - Нині",
+                    "title" => "Front-End Розробник",
+                    "desc" => "Спеціалізуюся на створенні адаптивних веб-сайтів для компаній різних розмірів та напрямків діяльності"
+                ],
+                [
+                    "year" => "2014 - Нині",
+                    "title" => "Web Дизайнер",
+                    "desc" => "Створюю лаконічні дизайни на різних платформах та розробляю шаблони для сервісу Bitrix24"
+                ]
+            )
+        ),
+        "contacts-title" => "Контакти",
+        "contacts-subtitle" => "Давайте співпрацювати прямо зараз!",
+        "contacts-text" => "Щоб зв'язатися зі мною швидше, напишіть мені в",
+        "form" => array(
+            "name" => "Ваше ім'я",
+            "message" => "Ваше повідомлення",
+            "button" => "Надіслати повідомлення"
+        )
+    )
+);
